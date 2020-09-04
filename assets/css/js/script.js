8263f488e5f90219020c06731ac2ff4e995f2eaf
cornwalljeremy <cornwall.jeremy@gmail.com>
2020-09-04 12:21:14 -0600

    layout

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,1 +1,10 @@
-var apiUrl = api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={4c2f8d8f735edeb060159e75322d57d5}
+var button = $(".btn");
+var zipInput = $(".form-control").val;
+var todaysDate = moment().format("LL");
+
+
+
+$("#time").text(todaysDate);
+console.log(todaysDate)
+
+//var apiUrl = api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={4c2f8d8f735edeb060159e75322d57d5}
